ca537912e5a57f2fd8d29f2a9ace5c856f1914d4
Shaq <shahak.kuba@connect.qut.edu.au>
2022-05-06 22:10:42 +1000

    changed from 4-D mat to 3-D mat

--- Data/dataFormatting.m
+++ Data/dataFormatting.m
@@ -3,8 +3,8 @@ clear all; close all; clc
 % 0 = no tumor, 1 = glioma, 2 = meningioma, 3 = pituitary
 data_label = [0,1,2,3];
 
-targetX = 64
-targetY = 64
+targetX = 64;
+targetY = targetX; % square image
 
 %% for training data
 glioma_images_tr = dir("Training\glioma_tumor\*.jpg");
@@ -21,15 +21,16 @@ tr_idx = 1;
 for i = 1:length(all_tr)
     label = i;
     files = all_tr{i};
-    for j = 1:length(files)
-        labels_tr(tr_idx) = label;
-        images_tr(:, :, :, tr_idx) = imresize(imread(strcat(files(j).folder, "/", files(j).name)), [targetX, targetY]);
+    for j = 1:50 %length(files)
+        labels_tr(tr_idx) = label - 1;
+        images_training(:, :, :, tr_idx) = imresize(imread(strcat(files(j).folder, "/", files(j).name)), [targetX, targetY]);
+        tumor_images_training(:,:, tr_idx) = images_training(:,:,1,tr_idx);
         tr_idx = tr_idx + 1;
     end
 end
 
 figure;
-imshow(images_tr(:,:,:,1))
+imshow(tumor_images_training(:,:,1))
 
 
 %% for testing data
@@ -45,18 +46,20 @@ te_idx = 1;
 for i = 1:length(all_te)
     label = i;
     files = all_te{i};
-    for j = 1:length(files)
-        labels_te(te_idx) = label;
-        images_te(:, :, :, te_idx) = imresize(imread(strcat(files(j).folder, "/", files(j).name)), [targetX, targetY]);
+    for j = 1:50 %length(files)
+        labels_te(te_idx) = label - 1;
+        images_testing(:, :, :, te_idx) = imresize(imread(strcat(files(j).folder, "/", files(j).name)), [targetX, targetY]);
+        tumor_images_testing(:,:,te_idx) = images_testing(:,:,1,te_idx);
         te_idx = te_idx + 1;
     end
 end
 
 figure;
-imshow(images_te(:,:,:,1))
+imshow(tumor_images_testing(:,:,1))
 
 
-%%
-save("data.mat", "labels_tr", "images_tr", "labels_te", "images_te");
+%% Saving the data
+save("tumor_training.mat", "labels_tr", "tumor_images_training");
+save("tumor_testing.mat", "labels_te", "tumor_images_testing");
 
 
